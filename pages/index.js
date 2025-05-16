759fe5bb80fe2bdee95a4d406ab41bdb99c037c2
me <me@localhost.localdomain>
2025-05-16 14:20:36 -0300

    Add pages/index.js so Next.js can build

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      height: '100vh',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '1.5rem'
+    }}>
+      👋 Welcome to Parfum Shop!
+    </div>
+  )
+}
